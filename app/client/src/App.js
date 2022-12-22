7bf42f72c1bd4accd70b39b4bcd09bc946556c4a
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2022-12-21 20:35:35 -0600

    auth login and register complete with formik

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -2,15 +2,12 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  // Navigate,
+  Navigate,
 } from "react-router-dom";
-
 // import { HomePage, LoginPage, ProfilePage } from "scenes/index/scenes.index";
-
-import HomePage from "scenes/homePage/HomePg"
-import LoginPage from "scenes/loginPage/LoginPg"
-import ProfilePage from "scenes/profilePage/ProfilePg"
-
+import HomePage from "scenes/homePage/HomePg";
+import LoginPage from "scenes/loginPage/LoginPg";
+import ProfilePage from "scenes/profilePage/ProfilePg";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@mui/material";
@@ -20,6 +17,7 @@ import { themeSettings } from "./theme";
 function App() {
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
 
   return (
     <div className="App">
@@ -28,8 +26,14 @@ function App() {
           <CssBaseline />
           <Routes>
             <Route path="/" element={<LoginPage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/profile/:userId" element={<ProfilePage />} />
+            <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
           </Routes>
         </ThemeProvider>
       </Router>
